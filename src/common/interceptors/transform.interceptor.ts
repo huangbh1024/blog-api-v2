9ba9a8f411cee5c22fe9a3b4255f7a713aafe4a2
huangbh <578092215@qq.com>
2024-03-27 15:42:34 +0800

    feat(common): :sparkles: 添加全局过滤器

---
+++ src/common/interceptors/transform.interceptor.ts
@@ -0,0 +1,34 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+export class TransformInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    const ctx = context.switchToHttp();
+    const request = ctx.getRequest();
+    return next.handle().pipe(
+      map((data) => ({
+        data,
+        code: 200,
+        message: 'success',
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      })),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,18 @@ import {
 } from '@nestjs/platform-fastify';
 import { VersioningType } from '@nestjs/common';
 
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
   app.enableVersioning({ defaultVersion: '2', type: VersioningType.URI });
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
