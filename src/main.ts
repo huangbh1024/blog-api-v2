e023fc17c70eefa8a5b23e56827ccfe53620d1cb
huangbh <578092215@qq.com>
2024-03-29 09:42:06 +0800

    feat(swagger): :sparkles: 添加swagger

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ import { VersioningType } from '@nestjs/common';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+
+import { generateDocument } from './doc';
+
 declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -18,10 +21,11 @@ async function bootstrap() {
   app.enableVersioning({ defaultVersion: '2', type: VersioningType.URI });
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
-  await app.listen(3000);
+  generateDocument(app);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+  await app.listen(3000);
 }
 bootstrap();
